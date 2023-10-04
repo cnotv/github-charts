485bb478336994f8652e127f7a44ed5b1ee5f8fa
cnotv <giuseppe.leo@suse.com>
2023-10-04 22:26:29 +0200

    Display Github list of bugs issues

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { Octokit } from "@octokit/core";
+import { ref } from 'vue'
+
+const openedIssues = ref({});
+
+const octokit = new Octokit({ auth: import.meta.env.API_TOKEN });
+const owner = 'rancher';
+const repo = 'dashboard';
+
+(async () => {
+  const resp = await octokit.request(`GET /repos/{owner}/{repo}/issues`, {
+    owner,
+    repo,
+    per_page: 100,
+    since: '2023-10-01',
+    sort: 'created',
+    labels: 'kind/bug',
+  });
+
+  openedIssues.value = resp.data.map(({url, number, title}) => ({
+    url, number, title
+  }));
+})()
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Opened Bug Issues: {{ openedIssues.length }}</h1>
+    <ul>
+      <li v-for="issue of openedIssues">
+        <a :href="issue.url" target="_blank">
+          #{{ issue.number }}: {{ issue.title }}
+        </a>
+      </li>
+    </ul>
   </main>
 </template>
